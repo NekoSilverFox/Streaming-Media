72cfe447cc962530dfa9b1fc11874ce4bb730d13
NekoSilverFox <weidufox@gmail.com>
2023-12-30 14:17:37 +0300

    优化 cpp 工程的头文件导入

--- 001_ffmpeg_qt_test/main.cpp
+++ 001_ffmpeg_qt_test/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-// 包含ffmpeg头文件
+// 包含ffmpeg头文件，MacOS 下测试必须加 extern "C" 不然报错
+#ifdef __cplusplus
 extern "C"{
 #include <libavutil/avutil.h>
 }
+#endif
 
 using namespace std;
 
